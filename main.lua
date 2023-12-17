8afc9f5915ee756e3bdca35d2a8d73beeb280093
Bug <77031032+0zBug@users.noreply.github.com>
2023-12-16 19:50:17 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,25 +1,45 @@
+
+local Element
+function Element(Parent)
+    return function(Class)
+        local Object = {}
+        local Children = {}
+
+        table.insert(Parent, Object)
+        
+        local new = Element(Children)
+
+        return setmetatable(Object, {
+            __index = function(self, index)
+                return (index == "new") and new or rawget(self, index)
+            end,
+            __newindex = function(self, index, value)
+                rawset(self, index:gsub("_", "-"), value)
+            end,
+            __tostring = function(self)
+                local Attributes = {}
+                local Descendants = {}
+
+                for Attribute, Value in next, Object do
+                    table.insert(Attributes, string.format("%s = \"%s\"", Attribute, Value))
+                end
+
+                for _, Child in next, Children do
+                    table.insert(Descendants, tostring(Child))
+                end
+
+                return string.format("<%s %s>%s</%s>", Class, table.concat(Attributes, " "), table.concat(Descendants, "\n"), Class)
+            end
+        })
+    end
+end
+
 return {
     new = function()
         local svg = {}
 
         return setmetatable({
-            new = function(Class)
-                local Object = {}
-
-                table.insert(svg, Object)
-
-                return setmetatable(Object, {
-                    __tostring = function(self)
-                        local Attributes = {}
-
-                        for Attribute, Value in next, Object do
-                            table.insert(Attributes, string.format("%s = \"%s\"", Attribute, Value))
-                        end
-
-                        return string.format("<%s %s/>", Class, table.concat(Attributes, " "))
-                    end
-                })
-            end
+            new = Element(svg)
         }, {
             __tostring = function()
                 local Objects = {}
